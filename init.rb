c296a42f70ef742aae94e27860113a051ede8898
Konstantin V. Borisov <smeagol74@gmail.com>
2020-01-21 13:16:06 +0700

    fix for Redmine 4.1

--- init.rb
+++ init.rb
@@ -10,7 +10,6 @@ Redmine::Plugin.register :redmine_emoji do
 end
 
 Rails.configuration.to_prepare do
-  Rails.configuration.assets.paths << Emoji.images_path
   Redmine::WikiFormatting.format_names.each do |format_name|
     formatter = Redmine::WikiFormatting.formatter_for(format_name)
     formatter.prepend(EmojiPlugin::FormatterWithEmoji)
